0a59d9963922a7a0395fb6aafba5d2617e9cf1de
Ed Tubbs <ectubbs@gmail.com>
2023-11-16 11:20:07 -0600

    Gitian build for 1.14.7

--- ci/ci.sh
+++ ci/ci.sh
@@ -3,6 +3,6 @@
 IMAGES=(ubuntu debian centos fedora archlinux)
 IMAGES=(ubuntu)
 for image in "${IMAGES[@]}"; do
-  docker build -t gitian_builder --build-arg IMAGE=${image}:latest .
+  docker build -t gitian_builder --build-arg IMAGE=${image}:focal .
   docker run -it -v $(pwd):/app --rm gitian_builder
 done
